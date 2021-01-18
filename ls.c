ddd887a5d994b79fd809a6a1f4b429a6b089cc1a
Arlandis Word <arlandis@remind101.com>
2021-01-17 20:14:22 -0500

    Support multiple dirs: ls foo bar

--- ls.c
+++ ls.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 #include <dirent.h>
 
-#define MAX_NUM_FILES 1000
+#define GRN "\e[0;32m"
+#define BLU "\e[0;34m"
+
+#define reset "\e[0m"
+
+#define DEFAULT_DIR "./"
 
 int print_dir(char* dirname){
+  printf(GRN "%s " reset "contents:\n", dirname);
   DIR *d = opendir(dirname);
   if (d == NULL)
   {
-    printf("Couldn't open dir: %s\n", dirname);
     return (1);
   }
   struct dirent *entry;
   while ((entry = readdir(d)) != NULL)
   {
-    printf("%s ", entry->d_name);
+    printf(BLU "%s " reset, entry->d_name);
   }
   printf("\n");
   (void) closedir(d);
@@ -27,16 +32,16 @@ void print_file(){
 
 int main(int argc, char **argv)
 {
-  char *dirname = "./";
-  if (argc == 2)
-  {
-    dirname = argv[1];
-    printf("argv: %s\n", argv[1]);
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      char *dirname = argv[i];
+      if (print_dir(dirname) != 0)
+      {
+        printf("Error printing dir: %s\n", dirname);
+      };
+    }
+  } else {
+    print_dir(DEFAULT_DIR);
   }
-  if (print_dir(dirname) != 0) {
-    printf("Error printing dir: %s\n", dirname);
-  };
-
-
   return 0;
 }
