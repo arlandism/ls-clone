c02049e0872327eda8a6d1aa1ceff0d307f1ccec
Arlandis Word <arlandis@remind101.com>
2021-01-17 22:22:56 -0500

    support size and modified_at

--- ls.c
+++ ls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 #define GRN "\e[0;32m"
 #define BLU "\e[0;34m"
@@ -8,6 +10,27 @@
 #define reset "\e[0m"
 
 #define DEFAULT_DIR "./"
+
+int print_file(char* dirname, struct dirent* entry){
+  int dirlen = strlen(dirname);
+  int pathlen = strlen(entry->d_name);
+  char path[dirlen + pathlen + 1];
+  int cx = snprintf(path, dirlen + pathlen + 2, "%s/%s", dirname, entry->d_name);
+  if (cx < 0 || cx >= dirlen + pathlen + 2) {
+    printf("%d\n", cx);
+    printf("Error getting pathname\n");
+    return -1;
+  }
+  struct stat * fstat = malloc(sizeof(stat));
+  if (stat(path, fstat) != 0) { //TODO: surely there is a better way to do this than make a syscall per file
+    printf("Error reading: %s\n", path);
+    free(fstat);
+    return -1;
+  }
+  printf(BLU "%8lld %ld %s\n" reset, fstat->st_size, fstat->st_atimespec.tv_nsec, entry->d_name);
+  free(fstat);
+  return 1;
+}
 
 int print_dir(char* dirname){
   printf(GRN "%s " reset "contents:\n", dirname);
@@ -19,15 +42,11 @@ int print_dir(char* dirname){
   struct dirent *entry;
   while ((entry = readdir(d)) != NULL)
   {
-    printf(BLU "%s " reset, entry->d_name);
+    print_file(dirname, entry);
   }
   printf("\n");
   (void) closedir(d);
   return 0;
-}
-
-void print_file(){
-
 }
 
 int main(int argc, char **argv)
